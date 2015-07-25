8ab7306aad8b4798f02393581784eaac98ec6932
Tinram <Tinram@users.noreply.github.com>
2015-07-25 11:57:31 +0100

    revise comments

--- mongobench.c
+++ mongobench.c
@@ -1,29 +1,30 @@
 
 /**
-	* Simple import test from stdin for MongoDB.
-	*
-	* Create a high number of very simple JSON within 1 second and pipe to mongoimport.
-	*
-	* Compile:
-	*                 GCC: gcc mongobench.c -o mb[.exe] -O3 -Wall -s
-	*                 TCC: mongobench.c mb[.exe]
-	* 
-	* Usage:
-	*                 mb[.exe] | mongoimport --db junk --drop junk --collection years
-	*
-	* Output:
-	*                 Windows:   insertion count, stop
-	*                 Linux:
-	*                            89900      29966/second
-	*                            210900     35150/second
-	*                            ...
-	*                            Ctrl + C   quit
-	*
-	* @author        Martin Latter, <copysense.co.uk>
-	* @copyright      Martin Latter, 25/07/2015
-	* @version       0.1
-	* @license        GNU GPL version 3.0 (GPL v3); http://www.gnu.org/licenses/gpl.html 
-	* @link           https://github.com/Tinram/Mongo-Bench.git
+    * Simple import test from stdin for MongoDB.
+    *
+    * Create a high number of very simple JSON lines within 1 second and pipe to the mongoimport command.
+    * JSON lines circumvent the current Mongo import limit of 4MB for a JSON array.
+    *
+    * Compile:
+    *                 GCC: gcc mongobench.c -o mb[.exe] -O3 -Wall -s
+    *                 TCC: mongobench.c mb[.exe]
+    *
+    * Usage:
+    *                 mb[.exe] | mongoimport --db junk --drop junk --collection years
+    *
+    * Output:
+    *                 Windows:   insertion count and time reported, stop.
+    *                 Linux:
+    *                            89900      29966/second
+    *                            210900     35150/second
+    *                            ...
+    *                            Ctrl + C   quit
+    *
+    * @author         Martin Latter, <copysense.co.uk>
+    * @copyright      Martin Latter, 24/07/2015
+    * @version        0.1
+    * @license        GNU GPL version 3.0 (GPL v3); http://www.gnu.org/licenses/gpl.html
+    * @link           https://github.com/Tinram/Mongo-Bench.git
 */
 
 
@@ -31,7 +32,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 clock_t tStart, tDiff;
+
 
 int main() {
 
@@ -47,14 +50,14 @@ int main() {
 
 		iYear = (rand() % 2000);
 
-		printf("{'year':'%d'}\n", iYear); // increase JSON complexity here
+		printf("{'year':'%d'}\n", iYear); // increase JSON schema complexity here
 
 		tDiff = clock() - tStart;
 		iMSec = tDiff * 1000 / CLOCKS_PER_SEC;
 
 		iCount++;
 
-	} while ((iMSec * 0.001) < 1); 
+	} while ((iMSec * 0.001) < 1);
 
 	/* uncomment the following two lines, compile, and redirect to a text file, and tail: see how many lines the program will print to file in 1 second */
 
